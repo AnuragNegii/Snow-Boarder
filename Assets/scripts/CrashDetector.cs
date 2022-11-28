368b4f31ec4262cdb4920262fdeaeb44aaa66678
AnuragNegii <negianurag99@gmail.com>
2022-11-28 10:30:36 +0530

    Level Reload and Finish line reload

--- Assets/scripts/CrashDetector.cs
+++ Assets/scripts/CrashDetector.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float levelLoad = 1f;
     private void OnTriggerEnter2D(Collider2D other) {
           if(other.tag == "object"){
-            Debug.Log("you lost");
+            Invoke("ReloadScene", levelLoad);
         }
     }
-
+    private void ReloadScene(){
+        SceneManager.LoadScene(0);
+    }
 }
