ee3157c3ae19cfb3eaf52c0f423b11015425c0f5
AnuragNegii <negianurag99@gmail.com>
2022-11-28 11:34:50 +0530

    added boost for player to use

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,39 @@ public class PlayerController : MonoBehaviour
 {   
     Rigidbody2D rb2d;
     [SerializeField] float torqueAmount  = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+    SurfaceEffector2D surfaceEffector2D;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
     
     void Update()
     {
-       if(Input.GetKey(KeyCode.LeftArrow)){
-        rb2d.AddTorque(torqueAmount);
-       }
-       else if(Input.GetKey(KeyCode.RightArrow)){
-        rb2d.AddTorque(-torqueAmount);
-       }
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+
+    void RespondToBoost(){
+        if(Input.GetKey(KeyCode.UpArrow)){
+            surfaceEffector2D.speed = boostSpeed;
+        }else{
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
     }
 }
